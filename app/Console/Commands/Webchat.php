718427537dcdd61673c2dc2b012dd78b92504143
chenalun <alun0104@foxmail.com>
2020-06-06 15:37:13 +0800

    ws

--- app/Console/Commands/Webchat.php
+++ app/Console/Commands/Webchat.php
@@ -21,6 +21,12 @@ class Webchat extends Command
     protected $description = 'Command description';
 
     /**
+     * websocket
+     * @var null
+     */
+    protected $server = NULL;
+
+    /**
      * Create a new command instance.
      *
      * @return void
@@ -28,6 +34,20 @@ class Webchat extends Command
     public function __construct()
     {
         parent::__construct();
+
+        $this->server = new \swoole_websocket_server("0.0.0.0", 9503);
+
+        // 心跳检测
+        //        $this->server->set(array(
+        //            'heartbeat_check_interval' => 5,  // 上面的设置就是每5秒侦测一次心跳
+        //            'heartbeat_idle_time' => 60,  // 一个TCP连接如果在10秒内未向服务器端发送数据，将会被切断。
+        //        ));
+
+        $this->server->on('open', [$this, 'open']);   //监听WebSocket连接打开事件
+        $this->server->on('message', [$this, 'message']); // 监听WebSocket消息事件
+        $this->server->on('close', [$this, 'close']);  // 监听WebSocket连接关闭事件
+
+        $this->server->start();
     }
 
     /**
@@ -37,28 +57,62 @@ class Webchat extends Command
      */
     public function handle()
     {
+//        if($this->argument('action') == 'start'){
+//
+//        }
+
         //
-        $ws = new \swoole_websocket_server("0.0.0.0", 9503);
-
-        //监听WebSocket连接打开事件
-        $ws->on('open', function ($ws, $request) {
-            var_dump($request->fd, $request->get, $request->server);
-            $ws->push($request->fd, "hello, welcome\n");
-        });
-
-        //监听WebSocket消息事件
-        $ws->on('message', function ($ws, $frame) {
-            echo "Message: {$frame->data}\n";
-            $ws->push($frame->fd, "server: {$frame->data}");
-        });
-
-        //监听WebSocket连接关闭事件
-        $ws->on('close', function ($ws, $fd) {
-            echo "client-{$fd} is closed\n";
-            $ws->push($fd, "client-{$fd} is closed!");
-        });
-        $ws->start();
+//        $ws = new \swoole_websocket_server("0.0.0.0", 9503);
+//
+//        //监听WebSocket连接打开事件
+//        $ws->on('open', function ($ws, $request) {
+//            var_dump($request->fd, $request->get, $request->server);
+//            $ws->push($request->fd, "hello, welcome\n");
+//        });
+//
+//        //监听WebSocket消息事件
+//        $ws->on('message', function ($ws, $frame) {
+//            echo "Message: {$frame->data}\n";
+//            $ws->push($frame->fd, "server: {$frame->data}");
+//        });
+//
+//        //监听WebSocket连接关闭事件
+//        $ws->on('close', function ($ws, $fd) {
+//            echo "client-{$fd} is closed\n";
+//            $ws->push($fd, "client-{$fd} is closed!");
+//        });
+//        $ws->start();
 
         $this->info("success action [{$this->argument('action')}]");
     }
+
+    /**
+     * 监听WebSocket连接打开事件
+     * @param $server
+     * @param $request
+     */
+    public function open($server, $request){
+        echo "connection open: {$request->fd}\n";
+        $server->push($request->fd, "hello, welcome\n");
+    }
+
+    /**
+     * 监听WebSocket消息事件
+     */
+    public function message($server, $frame){
+        echo "Message: {$frame->data}\n";
+        $server->push($frame->fd, "server: {$frame->data}");
+    }
+
+    /**
+     * 监听WebSocket连接关闭事件
+     * @param $server
+     * @param $fd
+     */
+    public function close($server, $fd){
+        $server->push($fd, "client-{$fd} is closed");
+        echo "client-{$fd} is closed\n";
+
+    }
+
 }
